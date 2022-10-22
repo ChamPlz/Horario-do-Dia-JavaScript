279a5cfc9dc7a9e26d4c21b1593a08914f2bccb9
Carlos Junior <49049655+ChamPlz@users.noreply.github.com>
2022-10-22 12:05:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function load(){
+    var msg = document.querySelector('#msg')
+    var img = document.querySelector('#imgd')
+    var data = new Date()
+    var hora =  21//data.getHours() 
+    
+    msg.innerHTML= `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        msg.innerHTML += ` Bom Dia!`
+        img.src = "./img/manha.png"
+        document.body.style.background = "#d4c667"
+    }else if(hora >= 12 && hora < 18){
+        msg.innerHTML += ` Boa Tarde!`
+        img.src = "./img/tarde.png"
+        document.body.style.background = "#f87c08"
+    }else if(hora >= 18){
+        msg.innerHTML += ` Boa Noite!`
+        img.src = "./img/noite.png"
+        document.body.style.background = "#002554"
+    }
+}
